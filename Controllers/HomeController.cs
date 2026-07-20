05801155d82863a8f1f8294990eb1cf33530a819
Satyam09958 <90625707+Satyam09958@users.noreply.github.com>
2026-07-21 00:59:51 +0530

    Add Method in Controler
    Add Method in Controler

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -24,6 +24,11 @@ namespace WebApplication1.Controllers
 
             return View();
         }
+        public IActionResult Add()
+        {
+
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
